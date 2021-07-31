23a70c0a721e9db954989d660718f8c2879f5d9e
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-07-31 10:26:26 -0300

    Add stations API

--- src/INMET.jl
+++ src/INMET.jl
@@ -1,5 +1,26 @@
 module INMET
 
-# Write your package code here.
+using HTTP
+using JSON
+using DataFrames
+
+"""
+    stations(kind=:T)
+
+Return INMET stations of given `kind`. There are two
+kinds of stations: automatic (`:T`) and manual (`:M`).
+"""
+function stations(kind=:T)
+  root = "https://apitempo.inmet.gov.br/estacoes/"
+  url  = root * String(kind)
+  page = HTTP.get(url)
+  str  = String(page.body)
+  data = JSON.parse(str)
+  vars = data |> first |> keys |> collect
+  cols = map(vars) do var
+    var => [d[var] for d in data]
+  end
+  DataFrame(cols)
+end
 
 end
