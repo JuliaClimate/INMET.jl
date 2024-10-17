aea7b87bdc04944f3a146dd73a4cd886ce32c0cb
Elias Carvalho <eliascarvdev@gmail.com>
2024-10-17 17:30:17 -0300

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,16 +13,25 @@ using Test
   end
 
   @testset "series" begin
-    df = INMET.series(:A301, Date(2021,1,1), Date(2021,7,31))
-    @test all(isequal("A301"), df.CD_ESTACAO)
+    if haskey(ENV, "INMET_TOKEN")
+      df = INMET.series(:A301, Date(2021,1,1), Date(2021,7,31))
+      @test all(isequal("A301"), df.CD_ESTACAO)
+    else
+      @test_throws ErrorException INMET.series(:A301, Date(2021,1,1), Date(2021,7,31))
+    end
   end
 
   @testset "on" begin
-    dfday  = INMET.on(Date(2021,7,1))
-    dfhour = INMET.on(DateTime(2021,7,1,1))
-    @test size(dfday, 1) > size(dfhour, 1)
-    @test all(isequal("2021-07-01"), dfday.DT_MEDICAO)
-    @test all(isequal("2021-07-01"), dfhour.DT_MEDICAO)
-    @test all(isequal("0100"), dfhour.HR_MEDICAO)
+    if haskey(ENV, "INMET_TOKEN")
+      dfday  = INMET.on(Date(2021,7,1))
+      dfhour = INMET.on(DateTime(2021,7,1,1))
+      @test size(dfday, 1) > size(dfhour, 1)
+      @test all(isequal("2021-07-01"), dfday.DT_MEDICAO)
+      @test all(isequal("2021-07-01"), dfhour.DT_MEDICAO)
+      @test all(isequal("0100"), dfhour.HR_MEDICAO)
+    else
+      @test_throws ErrorException INMET.on(Date(2021,7,1))
+      @test_throws ErrorException INMET.on(Date(2021,7,1))
+    end
   end
 end
